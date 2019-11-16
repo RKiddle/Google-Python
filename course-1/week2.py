fb617f08c6ca2af6360720a64b0ee039a01dcd73
RKiddle <RKiddle@users.noreply.github.com>
2019-11-16 18:11:15 +0700

    Create week2.py

---
+++ course-1/week2.py
@@ -0,0 +1,96 @@
+#1
+
+def color_translator(color):
+	if color == "red":
+		hex_color = "#ff0000"
+	elif color == "green":
+		hex_color = "#00ff00"
+	elif color == "blue":
+		hex_color = "#0000ff"
+	else:
+		hex_color = "unknown"
+	return hex_color
+
+print(color_translator("blue")) # Should be #0000ff
+print(color_translator("yellow")) # Should be unknown
+print(color_translator("red")) # Should be #ff0000
+print(color_translator("black")) # Should be unknown
+print(color_translator("green")) # Should be #00ff00
+print(color_translator("")) # Should be unknown
+
+#4
+
+def exam_grade(score):
+	if score > 95:
+		grade = "Top Score"
+	elif score >= 60:
+		grade = "Pass"
+	else:
+		grade = "Fail"
+	return grade
+
+print(exam_grade(65)) # Should be Pass
+print(exam_grade(55)) # Should be Fail
+print(exam_grade(60)) # Should be Pass
+print(exam_grade(95)) # Should be Pass
+print(exam_grade(100)) # Should be Top Score
+print(exam_grade(0)) # Should be Fail
+
+#6
+
+ddef format_name(first_name, last_name):
+	if first_name and last_name != "":
+	  print("Name: "+last_name + ", " + first_name)
+	elif first_name == "":
+	  print("Name: "+last_name)
+	elif last_name == "":
+	  print("Name: "+first_name)
+	elif first_name and last_name == "":
+	  print("") 
+	else:
+	  return
+
+print(format_name("Ernest", "Hemingway"))
+# Should be "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should be "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should be "Name: Voltaire"
+
+print(format_name("", ""))
+# Should be ""
+
+#7
+
+
+
+def longest_word(word1, word2, word3):
+	if len(word1) >= len(word2) and len(word1) >= len(word3):
+		word = word1
+	elif len(word2) >= len(word1) and len(word2) >= len(word3):
+		word = word2
+	else:
+		word = word3
+	return(word)
+
+print(longest_word("chair", "couch", "table"))
+print(longest_word("bed", "bath", "beyond"))
+print(longest_word("laptop", "notebook", "desktop"))
+
+#10
+
+def fractional_part(numerator, denominator):
+	if denominator != 0:
+	  remainder =  numerator/denominator
+	  return remainder - int(remainder)
+	else:
+	  return 0
+
+print(fractional_part(5, 5)) # Should be 0
+print(fractional_part(5, 4)) # Should be 0.25
+print(fractional_part(5, 3)) # Should be 0.66...
+print(fractional_part(5, 2)) # Should be 0.5
+print(fractional_part(5, 0)) # Should be 0
+print(fractional_part(0, 5)) # Should be 0
