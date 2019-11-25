56a145252db8b213f2bb83bc3b8b798d64505766
RKiddle <RKiddle@users.noreply.github.com>
2019-11-25 22:30:42 +0700

    Create Wk3_quiz2.py

---
+++ course-2/Wk3_quiz2.py
@@ -0,0 +1,42 @@
+#1
+
+import re
+def transform_record(record):
+  new_record = re.sub('')
+  return new_record
+
+print(transform_record("Sabrina Green,802-867-5309,System Administrator")) 
+# Sabrina Green,+1-802-867-5309,System Administrator
+
+print(transform_record("Eli Jones,684-3481127,IT specialist")) 
+# Eli Jones,+1-684-3481127,IT specialist
+
+print(transform_record("Melody Daniels,846-687-7436,Programmer")) 
+# Melody Daniels,+1-846-687-7436,Programmer
+
+print(transform_record("Charlie Rivera,698-746-3357,Web Developer")) 
+# Charlie Rivera,+1-698-746-3357,Web Developer
+
+
+#2
+
+import re
+def multi_vowel_words(text):
+  pattern = ('\B[aeiouAEIOU][aeiouAEIOU][aeiouAEIOU]')
+  result = re.findall(pattern, text)
+  return result
+
+print(multi_vowel_words("Life is beautiful")) 
+# ['beautiful']
+
+print(multi_vowel_words("Obviously, the queen is courageous and gracious.")) 
+# ['Obviously', 'queen', 'courageous', 'gracious']
+
+print(multi_vowel_words("The rambunctious children had to sit quietly and await their delicious dinner.")) 
+# ['rambunctious', 'quietly', 'delicious']
+
+print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)")) 
+# ['queue']
+
+print(multi_vowel_words("Hello world!")) 
+# []
