198d0acd7e4a0ab0dc79d99fe4ab07e7e23fb686
RKiddle <RKiddle@users.noreply.github.com>
2019-12-04 20:34:14 +0700

    Update Dictionaries_Practice_Quiz.py

--- course-1/Dictionaries_Practice_Quiz.py
+++ course-1/Dictionaries_Practice_Quiz.py
@@ -2,13 +2,12 @@
 
 def email_list(domains):
 	emails = []
-	for ___:
-	for user in users:
-			emails.___
+	for email_domains in domains.keys():
+	  for user in domains.values():
+		  emails.append(domains.zip())
 	return(emails)
 
 print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
-
 
 #2
 
