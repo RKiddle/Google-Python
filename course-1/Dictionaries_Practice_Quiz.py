380a49b1f24b98a6460113ecc00def44cd570ce8
RKiddle <RKiddle@users.noreply.github.com>
2019-12-04 20:07:43 +0700

    Create Dictionaries_Practice_Quiz.py

---
+++ course-1/Dictionaries_Practice_Quiz.py
@@ -0,0 +1,29 @@
+#1
+
+def email_list(domains):
+	emails = []
+	for ___:
+	for user in users:
+			emails.___
+	return(emails)
+
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+
+
+#2
+
+def groups_per_user(group_dictionary):
+	user_groups = {}
+	# Go through group_dictionary
+	for ___:
+		# Now go through the users in the group
+		for ___:
+			# Now add the group to the the list of
+# groups for this user, creating the entry
+# in the dictionary if necessary
+
+	return(user_groups)
+
+print(groups_per_user({"local": ["admin", "userA"],
+		"public":  ["admin", "userB"],
+		"administrator": ["admin"] }))
