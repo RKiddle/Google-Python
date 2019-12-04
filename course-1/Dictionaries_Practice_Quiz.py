bcce10a30ca6d7a90dc318ce21b3e4669d30d753
RKiddle <RKiddle@users.noreply.github.com>
2019-12-04 21:44:03 +0700

    Update Dictionaries_Practice_Quiz.py

--- course-1/Dictionaries_Practice_Quiz.py
+++ course-1/Dictionaries_Practice_Quiz.py
@@ -14,9 +14,10 @@ print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "
 def groups_per_user(group_dictionary):
 	user_groups = {}
 	# Go through group_dictionary
-	for ___:
+	for keys in group_dictionary:
 		# Now go through the users in the group
-		for ___:
+		for values in group_dictionary[keys]:
+		  user_groups[keys] = [values]
 			# Now add the group to the the list of
 # groups for this user, creating the entry
 # in the dictionary if necessary
@@ -26,3 +27,5 @@ def groups_per_user(group_dictionary):
 print(groups_per_user({"local": ["admin", "userA"],
 		"public":  ["admin", "userB"],
 		"administrator": ["admin"] }))
+
+#both are wrong.
