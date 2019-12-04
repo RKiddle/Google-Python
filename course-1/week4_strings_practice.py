61118a0621d8182b4399feb38bf45896e9f0644f
RKiddle <RKiddle@users.noreply.github.com>
2019-12-04 20:05:07 +0700

    Update week4_strings_practice.py

--- course-1/week4_strings_practice.py
+++ course-1/week4_strings_practice.py
@@ -1,26 +1,20 @@
 #1
-
 def is_palindrome(input_string):
 
 # make it suitable for caseless comparison
 
 #remove whitespaces
-  input_string.replace(" ", "")
+  input_string2 = input_string.replace(" ", "")
 
-  input_string.lower().strip()
-  input_string = list(input_string)
+  input_string3 = input_string2.lower().strip()
+  input_string4 = list(input_string3)
 # reverse the string
-  rev_str = input_string[::-1]
+  rev_str = input_string4[::-1]
 # check if the string is equal to its reverse
-  if input_string == rev_str:
+  if input_string4 == rev_str:
    return True
   else:
    return False
-  	
-
-print(is_palindrome("Never Odd or Even")) # Should be True #my program says false
-print(is_palindrome("abc")) # Should be False
-print(is_palindrome("kayak")) # Should be True
 
 
 #2
