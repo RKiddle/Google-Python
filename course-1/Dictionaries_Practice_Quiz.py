9fa2b481345487350eae3a801756e980be30d68b
RKiddle <RKiddle@users.noreply.github.com>
2019-12-08 16:14:57 +0700

    Update Dictionaries_Practice_Quiz.py

--- course-1/Dictionaries_Practice_Quiz.py
+++ course-1/Dictionaries_Practice_Quiz.py
@@ -4,11 +4,10 @@ def email_list(domains):
 	emails = []
 	for keys in domains:
 	  for values in domains[keys]:
-		  emails= print(values+'@'+keys)
+		  emails.append(values+'@'+keys)
 	return(emails)
 
 print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
-
 #2
 
 def groups_per_user(group_dictionary):
@@ -17,7 +16,7 @@ def groups_per_user(group_dictionary):
 	for keys in group_dictionary:
 		# Now go through the users in the group
 		for values in group_dictionary[keys]:
-		  user_groups[keys] = [values]
+		  user_groups.update({keys:values})
 			# Now add the group to the the list of
 # groups for this user, creating the entry
 # in the dictionary if necessary
@@ -28,4 +27,4 @@ print(groups_per_user({"local": ["admin", "userA"],
 		"public":  ["admin", "userB"],
 		"administrator": ["admin"] }))
 
-#both are wrong.
+
